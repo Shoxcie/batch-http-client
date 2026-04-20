a9add745df99c82c954c58c4139dc51985266e22
Shoxcie <shoxcie@gmail.com>
2026-04-20 19:30:51 +0300

    refactor!: remove deprecated camelCase function aliases

--- src/Response.php
+++ src/Response.php
@@ -9,68 +9,6 @@ use Symfony\Contracts\HttpClient\Exception\ExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 use Symfony\Contracts\HttpClient\ResponseInterface;
 
-#[Deprecated(message: 'use get_url() instead')]
-function getUrl(ResponseInterface $response): string
-{
-    /** @var string */
-    return $response->getInfo('url');
-}
-
-#[Deprecated(message: 'use get_total_time() instead')]
-function getTotalTime(ResponseInterface $response): float
-{
-    /** @var float */
-    return $response->getInfo('total_time');
-}
-
-#[Deprecated(message: 'use get_status_code() instead')]
-function getStatusCode(ResponseInterface $response, bool $throw = false): int
-{
-    try {
-        return $response->getStatusCode();
-
-    } catch (TransportExceptionInterface $e) {
-        if ($throw) {
-            throw $e;
-        }
-    }
-
-    return 0;
-}
-
-/**
- * @return array<string, mixed>
- */
-#[Deprecated(message: 'use get_headers() instead')]
-function getHeaders(ResponseInterface $response, bool $throw = false): array
-{
-    try {
-        return $response->getHeaders();
-
-    } catch (ExceptionInterface $e) {
-        if ($throw) {
-            throw $e;
-        }
-    }
-
-    return [];
-}
-
-#[Deprecated(message: 'use get_content() instead')]
-function getContent(ResponseInterface $response, bool $throw = false): string
-{
-    try {
-        return $response->getContent();
-
-    } catch (ExceptionInterface $e) {
-        if ($throw) {
-            throw $e;
-        }
-    }
-
-    return '';
-}
-
 #[Deprecated(message: "passing custom user_data won't be allowed")]
 function getUserData(ResponseInterface $response): mixed
 {
